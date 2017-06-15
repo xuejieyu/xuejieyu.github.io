5c908ce22b93aa96d5e32a2ecfda42ad7ee4a3cd
xuejieyu <xuejieyu@gmail.com>
2017-06-14 23:25:27 -0700

    update infomation

--- build_site.R
+++ build_site.R
@@ -2,10 +2,12 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/coolfish/Documents/gittub/personal_site/my_site")
+setwd("/Users/coolfish/Documents/github/personal_site/my_site")
 
 #render your sweet site. 
 rmarkdown::render_site()
+
+
 
 
 
